50cfcb09b44b92e48308320ed94c2cdba25dac71
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-12-11 10:34:47 +0530

    Added Java related Notes

---
+++ 01 Basic Exercise/01 Hello World/Main.java
@@ -0,0 +1,27 @@
+// Main.java
+
+/*
+public :: Scope 
+class :: Blueprint for creating objects
+Main :: Name of the class
+*/
+public class Main {
+    /*
+    public :: Scope
+    static :: Belongs to the class rather than instances
+    void :: No return value
+    main :: Entry point of the program
+    String[] args :: Parameter for command-line arguments
+    */
+    public static void main(String[] args) {
+        // Comment: Print Hello, World! to the console
+
+        /*
+        System :: Standard system class
+        out :: Standard output stream
+        println :: Method to print a line of text
+        "Hello, World!" :: Text to be printed
+        */
+        System.out.println("Hello, World!");
+    }
+}
